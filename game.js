6af30248d6ec07592cb194938f968aa86b02524e
francisvimal2002 <francisvimal2002@gmail.com>
2025-03-29 13:50:26 +0000

    002 - userbuttonclicked event

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 var randomChosenColor, randomNumber;
 var gamePattern = [];
-
+var userClicked;
 function nextSequence() {
     randomNumber = Math.floor(Math.random()  * 4);
     randomChosenColor = buttonColors[randomNumber];
@@ -13,11 +13,18 @@ function nextSequence() {
 nextSequence();
 
 var selectedElement = $("#" + randomChosenColor);
-selectedElement.on("click", function () {
-    console.log('this', this)
-    selectedElement.toggleClass("pressed");
-})
+selectedElement.on("click", buttonEvent);
 
 function buttonEvent() {
-
+    // console.log('this', this);
+    selectedElement.fadeOut(100).fadeIn(100);
+    const sound = new Audio(`./sounds/${randomChosenColor}.mp3`);
+    console.log("audio: ", sound)
+    sound.play();
 }
+
+const buttonElement = $(".btn");
+buttonElement.on("click", function () {
+    userClicked = $(this).attr('id');
+    console.log(userClicked);
+});
