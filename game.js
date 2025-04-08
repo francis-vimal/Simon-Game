ee9b8f9bcac9376266755e3708fc6da59fe084e4
francisvimal2002 <francisvimal2002@gmail.com>
2025-04-08 15:32:05 +0000

    004 - Added full game functionality

--- game.js
+++ game.js
@@ -4,32 +4,61 @@ var randomChosenColor, randomNumber;
 var gamePattern = [];
 var userClicked;
 var userClickedPattern = [];
+var selectedElement;
+var level = 0;
+
+$(".btn").on("click", function () {
+    userClicked = $(this);
+    userClickedPattern.push(userClicked.attr('id'));
+    animatePress(userClicked.attr('id'));
+    playSound(userClicked.attr('id'));
+    console.log("userClickedPattern", userClickedPattern);
+    if(userClickedPattern.every((value, index) => value === gamePattern[index])) {
+        if(userClickedPattern.length === gamePattern.length) {
+            nextSequence();
+        }
+    }
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 500);
+        $("#level-title").text("Game Over - Press any key to restart");
+        level = 0;
+        userClickedPattern = [];
+        gamePattern = [];
+    }
+});
+
+$("body").keypress(nextSequence);
+
 function nextSequence() {
+    $("#level-title").text("Level " + (++level));
+    userClickedPattern = [];
     randomNumber = Math.floor(Math.random()  * 4);
     randomChosenColor = buttonColors[randomNumber];
-    console.log(randomNumber, randomChosenColor);
+    selectedElement = $("#" + randomChosenColor);
+    setTimeout(() => {
+        buttonEvent();
+    }, 500);
     gamePattern.push(randomChosenColor);
+    console.log("gamePattern", gamePattern);
 }
 
-nextSequence();
-
-var selectedElement = $("#" + randomChosenColor);
-selectedElement.on("click", buttonEvent);
-
 function buttonEvent() {
-    // console.log('this', this);
     selectedElement.fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
 }
 
-$(".btn").on("click", function () {
-    userClicked = $(this).attr('id');
-    userClickedPattern.push(userClicked);
-    playSound(userClicked);
-    console.log(userClickedPattern);
-});
-
 function playSound(color) {
     const sound = new Audio(`./sounds/${color}.mp3`);
     sound.play();
+}
+
+function animatePress(currentColour) {
+    userClicked.addClass('pressed');
+    setTimeout(() => {
+        userClicked.removeClass('pressed')
+    }, 100);
 }
